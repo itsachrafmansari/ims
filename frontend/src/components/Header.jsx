5ebeb98350b97dd466742f2b3c70dbc63f9ac6d0
itsachrafmansari <contact@achrafmansari.com>
2024-06-14 00:46:11 +0100

    Changed the page title underline image

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,13 +1,13 @@
 import Separator from "../assets/separator.svg";
-import triangleXO from "../assets/triangleXO.svg";
+import Binary1337 from "../assets/Binary1337.svg";
 
 const Header = ({ title, backgroundSource }) => {
     return (
         <header className="relative h-screen overflow-hidden flex flex-col justify-center items-center">
 
             <div className="z-[2] flex flex-col justify-center gap-3" >
-                <h1 className="text-white font-bold text-3xl">{title}</h1>
-                <img src={triangleXO} className="h-3" />
+                <h1 className="text-center text-3xl text-white font-bold">{title}</h1>
+                <Binary1337 className="h-2 w-auto fill-mainColor"/>
             </div>
 
             <div className="absolute z-[0] w-full h-full backdrop-filter backdrop-blur-sm backdrop-brightness-[0.35]"></div>
